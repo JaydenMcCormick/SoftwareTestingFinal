b163ea19c9f6d7aae3aadc6fae100ca77f4447e6
JMcCormick <jemccormick8714@eagle.fgcu.edu>
2024-04-18 05:22:44 -0400

    finished testing of live page

--- src/test/java/org/example/LivePage.java
+++ src/test/java/org/example/LivePage.java
@@ -14,7 +14,47 @@ public class LivePage {
 
     @Test
     public void testHomepage() throws InterruptedException {
+        chromeDriver.get("https://youtube.com/");
+        chromeDriver.manage().window().maximize();
 
+        Thread.sleep(1500);
+
+        // Open live tab on left banner, for some reason xpath wouldn't work for any i could find
+        WebElement live = chromeDriver.findElement(By.linkText("Live"));
+        live.click();
+
+        Thread.sleep(3500);
+
+        // click show more, also scrolls down automatically
+        WebElement showMore = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[1]/div/ytd-rich-shelf-renderer/div[1]/div[3]/ytd-button-renderer/yt-button-shape/button"));
+        showMore.click();
+
+        Thread.sleep(3000);
+
+        // click show less
+        WebElement showLess = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[1]/div/ytd-rich-shelf-renderer/div[1]/div[4]/ytd-button-renderer/yt-button-shape/button"));
+        showLess.click();
+
+        Thread.sleep(1000);
+
+        // scroll back up to top of page
+        JavascriptExecutor js = (JavascriptExecutor) chromeDriver;
+        js.executeScript("window.scrollTo(0, 0)");
+
+        Thread.sleep(1500);
+
+        // click view all to view all upcoming livestreams
+        WebElement viewAll = chromeDriver.findElement(By.linkText("View all"));
+        js.executeScript("arguments[0].click();", viewAll);
+
+
+        Thread.sleep(3000);
+
+        // click on first video, first upcoming livestream
+        WebElement firstVideo = chromeDriver.findElement(By.xpath("(//a[@id='video-title-link' and contains(@class, 'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media')])[1]"));
+        js.executeScript("arguments[0].click();", firstVideo);
+
+        Thread.sleep(5000);
     }
 
     @AfterMethod
