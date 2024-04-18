772de972e176049bc31056e8b582ba9dc0532fac
JMcCormick <jemccormick8714@eagle.fgcu.edu>
2024-04-18 02:34:06 -0400

    fully test searching

--- src/test/java/org/example/SearchPage.java
+++ src/test/java/org/example/SearchPage.java
@@ -14,7 +14,111 @@ public class SearchPage {
 
     @Test
     public void testHomepage() throws InterruptedException {
+        chromeDriver.get("https://youtube.com/");
+        Thread.sleep(1000);
 
+        chromeDriver.manage().window().maximize();
+
+//        // Find the email text box and enter the email
+//        WebElement loginButton = chromeDriver.findElement(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]"));
+//        loginButton.click();
+//
+//        WebElement emailInput = chromeDriver.findElement(By.xpath("//*[@id=\"identifierId\"]"));
+//        emailInput.sendKeys("j9095327@gmail.com");
+//        Thread.sleep(1000);
+//
+//        // Find the next button and click it
+//        WebElement emailNext = chromeDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span"));
+//        emailNext.click();
+//
+//        Thread.sleep(5000);
+//
+//        // Find the password text box and enter the password
+//        WebElement passInput = chromeDriver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+//        passInput.sendKeys("QwertyuioP1234567890!");
+//        Thread.sleep(1000);
+//
+//        // Find the next button and click it
+//        WebElement passNext = chromeDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/span"));
+//        passNext.click();
+//
+//        Thread.sleep(3500);
+
+        // Find search bar and click into it
+        WebElement searchBar = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"));
+        searchBar.click();
+
+        Thread.sleep(1000);
+
+        // Search for the organic chemistry tutor
+        searchBar.sendKeys("The Organic Chemistry Tutor");
+        Thread.sleep(1000);
+
+        // Press enter key
+        searchBar.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3500);
+
+        WebElement shorts = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[1]/yt-chip-cloud-renderer/div/div[2]/iron-selector/yt-chip-cloud-chip-renderer[2]/yt-formatted-string"));
+        shorts.click();
+
+        Thread.sleep(2000);
+
+        WebElement videos = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[1]/yt-chip-cloud-renderer/div/div[2]/iron-selector/yt-chip-cloud-chip-renderer[3]/yt-formatted-string"));
+        videos.click();
+
+        Thread.sleep(2000);
+
+        WebElement unwatched = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[1]/yt-chip-cloud-renderer/div/div[2]/iron-selector/yt-chip-cloud-chip-renderer[4]/yt-formatted-string"));
+        unwatched.click();
+
+        Thread.sleep(2000);
+
+        WebElement watched = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[1]/yt-chip-cloud-renderer/div/div[2]/iron-selector/yt-chip-cloud-chip-renderer[5]/yt-formatted-string"));
+        watched.click();
+
+        Thread.sleep(2000);
+
+        WebElement all = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[1]/yt-chip-cloud-renderer/div/div[2]/iron-selector/yt-chip-cloud-chip-renderer[1]/yt-formatted-string"));
+        all.click();
+
+        Thread.sleep(1250);
+
+        WebElement filtersButton = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[3]/ytd-button-renderer/yt-button-shape/button"));
+        filtersButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement uploadDate = chromeDriver.findElement(By.xpath("/html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog/ytd-search-filter-options-dialog-renderer/div[2]/ytd-search-filter-group-renderer[5]/ytd-search-filter-renderer[2]/a"));
+        uploadDate.click();
+
+        Thread.sleep(4000);
+
+        WebElement filtersButton1 = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[3]/ytd-button-renderer/yt-button-shape/button"));
+        filtersButton1.click();
+
+        Thread.sleep(2000);
+
+        WebElement durationButton = chromeDriver.findElement(By.xpath("/html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog/ytd-search-filter-options-dialog-renderer/div[2]/ytd-search-filter-group-renderer[3]/ytd-search-filter-renderer[1]/a"));
+        durationButton.click();
+
+        Thread.sleep(2000);
+
+        JavascriptExecutor js =(JavascriptExecutor) chromeDriver;
+        js.executeScript("window.scrollBy(0, 710)");
+
+        Thread.sleep(4000);
+
+        WebElement closeSearch = chromeDriver.findElement(By.xpath("//*[@id=\"search-clear-button\"]/ytd-button-renderer/yt-button-shape/button"));
+        closeSearch.click();
+
+        Thread.sleep(3000);
+
+        searchBar.sendKeys("Selenium installation");
+        Thread.sleep(1000);
+        searchBar.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3500);
     }
 
     @AfterMethod
