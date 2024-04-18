21e953a06f271fcec7dd921e7fd4f144357fc4a9
JEMcCormick <jemccormick8714@eagle.fgcu.edu>
2024-04-17 22:23:09 -0400

    template java classes for remaining test pages

---
+++ src/test/java/org/example/Subscriptions.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class Subscriptions {
+    private WebDriver chromeDriver;
+
+    @BeforeMethod
+    public void setUp() {
+        chromeDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void testHomepage() throws InterruptedException {
+
+    }
+
+    @AfterMethod
+    public void endTest() {
+        // Close the Chrome tab after the test is finished
+        chromeDriver.close();
+    }
+}
