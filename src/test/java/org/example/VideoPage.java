9b77b32453148130e674bd2c5bbc76d86573601f
JMcCormick <jemccormick8714@eagle.fgcu.edu>
2024-04-18 05:27:14 -0400

    parts i worked on for video page, just in case we need it after i leave pc

--- src/test/java/org/example/VideoPage.java
+++ src/test/java/org/example/VideoPage.java
@@ -14,12 +14,45 @@ public class VideoPage {
 
     @Test
     public void testHomepage() throws InterruptedException {
-        chromeDriver.get("https://www.youtube.com/watch?v=HaHsqDjWMLU");
+        chromeDriver.get("https://www.youtube.com/watch?v=t7xbCs4PrDk%22");
+        Thread.sleep(2500);
+
+        chromeDriver.manage().window().maximize();
+
+        // Find the email text box and enter the email
+        WebElement loginButton = chromeDriver.findElement(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]"));
+        loginButton.click();
+
+        WebElement emailInput = chromeDriver.findElement(By.xpath("//*[@id=\"identifierId\"]"));
+        emailInput.sendKeys("j9095327@gmail.com");
+        Thread.sleep(1000);
+
+        // Find the next button and click it
+        WebElement emailNext = chromeDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span"));
+        emailNext.click();
+
+        Thread.sleep(5000);
+
+        // Find the password text box and enter the password
+        WebElement passInput = chromeDriver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+        passInput.sendKeys("QwertyuioP1234567890!");
+        Thread.sleep(1000);
+
+        // Find the next button and click it
+        WebElement passNext = chromeDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/span"));
+        passNext.click();
+
+        Thread.sleep(7000);
+        JavascriptExecutor js =(JavascriptExecutor) chromeDriver;
+        js.executeScript("window.scrollBy(0, 500)");
+        Thread.sleep(2500);
+        WebElement comment = chromeDriver.findElement(By.cssSelector("ytd-comments ytd-comment-simplebox-renderer div#placeholder-area"));
+        comment.click();
+
+        comment = chromeDriver.findElement(By.cssSelector("#contenteditable-root"));
 
         Thread.sleep(2000);
-
-        WebElement videoPlayer = chromeDriver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[2]/button[9]"));
-        videoPlayer.click();
+        comment.sendKeys("hi");
         Thread.sleep(2000);
 
     }
