ace869fbad7f61fe4cf1710f4100d16cf462653b
JMcCormick <jemccormick8714@eagle.fgcu.edu>
2024-04-18 03:00:54 -0400

    fully test searching

--- src/test/java/org/example/SearchPage.java
+++ src/test/java/org/example/SearchPage.java
@@ -19,31 +19,6 @@ public class SearchPage {
 
         chromeDriver.manage().window().maximize();
 
-//        // Find the email text box and enter the email
-//        WebElement loginButton = chromeDriver.findElement(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]"));
-//        loginButton.click();
-//
-//        WebElement emailInput = chromeDriver.findElement(By.xpath("//*[@id=\"identifierId\"]"));
-//        emailInput.sendKeys("j9095327@gmail.com");
-//        Thread.sleep(1000);
-//
-//        // Find the next button and click it
-//        WebElement emailNext = chromeDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span"));
-//        emailNext.click();
-//
-//        Thread.sleep(5000);
-//
-//        // Find the password text box and enter the password
-//        WebElement passInput = chromeDriver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
-//        passInput.sendKeys("QwertyuioP1234567890!");
-//        Thread.sleep(1000);
-//
-//        // Find the next button and click it
-//        WebElement passNext = chromeDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/span"));
-//        passNext.click();
-//
-//        Thread.sleep(3500);
-
         // Find search bar and click into it
         WebElement searchBar = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"));
         searchBar.click();
