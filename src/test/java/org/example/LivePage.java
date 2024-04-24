5462e1da111ccd02f4561fee4f4c6edb2c7c6370
JMcCormick <jemccormick8714@eagle.fgcu.edu>
2024-04-23 22:13:08 -0400

    finishing touches, editing xpaths for my video

--- src/test/java/org/example/LivePage.java
+++ src/test/java/org/example/LivePage.java
@@ -26,13 +26,13 @@ public class LivePage {
         Thread.sleep(3500);
 
         // click show more, also scrolls down automatically
-        WebElement showMore = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[1]/div/ytd-rich-shelf-renderer/div[1]/div[3]/ytd-button-renderer/yt-button-shape/button"));
+        WebElement showMore = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[2]/div/ytd-rich-shelf-renderer/div[1]/div[3]/ytd-button-renderer/yt-button-shape/button"));
         showMore.click();
 
         Thread.sleep(3000);
 
         // click show less
-        WebElement showLess = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[1]/div/ytd-rich-shelf-renderer/div[1]/div[4]/ytd-button-renderer/yt-button-shape/button"));
+        WebElement showLess = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-section-renderer[2]/div/ytd-rich-shelf-renderer/div[1]/div[4]/ytd-button-renderer/yt-button-shape/button"));
         showLess.click();
 
         Thread.sleep(1000);
@@ -51,7 +51,7 @@ public class LivePage {
         Thread.sleep(3000);
 
         // click on first video, first upcoming livestream
-        WebElement firstVideo = chromeDriver.findElement(By.xpath("(//a[@id='video-title-link' and contains(@class, 'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media')])[1]"));
+        WebElement firstVideo = chromeDriver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse[2]/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row/div/ytd-rich-item-renderer[1]/div/ytd-rich-grid-media/div[1]/div[3]/div[1]/h3/a"));
         js.executeScript("arguments[0].click();", firstVideo);
 
         Thread.sleep(5000);
--- src/test/java/org/example/Video_Player.java
+++ src/test/java/org/example/Video_Player.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.awt.*;
@@ -22,26 +20,80 @@ public class Video_Player {
         driver.get("https://www.youtube.com/watch?v=BR2VC6d53D4");
     }
 
-    @Test(description = "Test 1 Play/Pause")
+    @Test(priority = 1, description = "Test 1 Play/Pause")
     public void play_pause() throws InterruptedException {
-        Thread.sleep(7000);
-        WebElement activate = driver.findElement(By.xpath("//*[@id=\"player\"]"));
+        Thread.sleep(2000);
+        WebElement activate = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[1]/button"));
         activate.click();
-        Thread.sleep(3000);
+        Thread.sleep(1500);
+        activate.click();
+        Thread.sleep(1500);
     }
 
-    @Test(description = "Test 2 Fullscreen")
+    @Test(priority = 2, description =  "Test 2 Fullscreen")
     public void Fullscreen() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         WebElement full = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[2]/button[9]"));
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         full.click();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
+        full.click();
+        Thread.sleep(2000);
     }
 
-    @Test(description = "Test 3 Commenting")
+    @Test(priority = 3, description =  "Test 3 Volume")
+    public void Volume() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement Vol = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[1]/span/button"));
+        Thread.sleep(2000);
+        Vol.click();
+        Thread.sleep(2000);
+        Vol.click();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 4, description =  "Test 4 liking a video")
+    public void Like_vid() throws InterruptedException {
+        WebElement like = driver.findElement(By.xpath("//*[@id=\"top-level-buttons-computed\"]/segmented-like-dislike-button-view-model/yt-smartimation/div/div/like-button-view-model/toggle-button-view-model/button-view-model"));
+        like.click();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 5, description =  "Test 5 Disliking a video")
+    public void dislike_vid() throws InterruptedException {
+        WebElement dislike = driver.findElement(By.xpath("//*[@id=\"top-level-buttons-computed\"]/segmented-like-dislike-button-view-model/yt-smartimation/div/div/dislike-button-view-model/toggle-button-view-model/button-view-model/button"));
+        dislike.click();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 6, description =  "Test 6 Add video to new playlist")
+    public void playlist() throws InterruptedException {
+        WebElement options = driver.findElement(By.xpath("//*[@id=\"flexible-item-buttons\"]/yt-button-view-model/button-view-model/button"));
+        options.click();
+        Thread.sleep(2000);
+    }
+
+
+    @Test(priority = 7, description =  "Test 7 Captions")
+    public void captions() throws InterruptedException {
+        WebElement caption = driver.findElement(By.cssSelector("button.ytp-subtitles-button"));
+        caption.click();
+        Thread.sleep(3500);
+    }
+
+    @Test(priority = 8, description =  "Test 8 Theatre mode")
+    public void Theatre() throws InterruptedException {
+        WebElement theatre = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[2]/button[7]"));
+        theatre.click();
+        Thread.sleep(1500);
+        theatre.click();
+        //sometimes div[29] works, sometimes div[33] works idk why
+    }
+
+    @Test(priority = 9, description =  "Test 9 Commenting")
     public void Commenting() throws InterruptedException {
-        Thread.sleep(7000);
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
         // Find the email text box and enter the email
         WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]"));
         loginButton.click();
@@ -65,7 +117,7 @@ public class Video_Player {
         WebElement passNext = driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/span"));
         passNext.click();
 
-        Thread.sleep(7000);
+        Thread.sleep(3500);
         JavascriptExecutor js =(JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0, 500)");
         Thread.sleep(2500);
@@ -83,57 +135,26 @@ public class Video_Player {
 
     }
 
-    @Test(description = "Test 4 Volume")
-    public void Volume() throws InterruptedException {
-        Thread.sleep(5000);
-        WebElement Vol = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[1]/span/button"));
-        Thread.sleep(2000);
-        Vol.click();
-    }
-
-    @Test(description = "Test 5 liking a video")
-    public void Like_vid() throws InterruptedException {
-        Thread.sleep(7000);
-        WebElement like = driver.findElement(By.xpath("//*[@id=\"top-level-buttons-computed\"]/segmented-like-dislike-button-view-model/yt-smartimation/div/div/like-button-view-model/toggle-button-view-model/button-view-model"));
-        like.click();
-    }
-
-    @Test(description = "Test 6 Disliking a video")
-    public void dislike_vid() throws InterruptedException {
-        Thread.sleep(7000);
-        WebElement dislike = driver.findElement(By.xpath("//*[@id=\"top-level-buttons-computed\"]/segmented-like-dislike-button-view-model/yt-smartimation/div/div/dislike-button-view-model/toggle-button-view-model/button-view-model/button"));
-        dislike.click();
-    }
-
-    @Test(description = "Test 7 Add video to new playlist")
-    public void playlist() throws InterruptedException {
-        Thread.sleep(7000);
-        WebElement options = driver.findElement(By.xpath("//*[@id=\"button-shape\"]/button"));
-        options.click();
-        Thread.sleep(2000);
-        WebElement save = driver.findElement(By.xpath("//*[@id=\"items\"]/ytd-menu-service-item-renderer[3]"));//if signed in, its renderer[3]
-        save.click();
-    }
-
-    @Test(description = "Test 8 Subscribe")
+    @Test(priority = 10, description =  "Test 10 Subscribe")
     public void subscribe() throws InterruptedException {
-        Thread.sleep(7000);
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 0)");
+        Thread.sleep(500);
         WebElement sub = driver.findElement(By.xpath("//*[@id=\"subscribe-button-shape\"]/button"));
         sub.click();
+        Thread.sleep(3500);
     }
 
-    @Test(description = "Test 9 Captions")
-    public void captions() throws InterruptedException {
-        Thread.sleep(7000);
-        WebElement caption = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[28]/div[2]/div[2]/button[3]"));
-        caption.click();
-    }
-
-    @Test(description = "Test 10 Mini_player")
+    @Test(priority = 11, description =  "Test 11 Mini_player")
     public void mini_player() throws InterruptedException {
-        Thread.sleep(7000);
+        WebElement video = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[1]/video"));
+        video.click();
+        Thread.sleep(1000);
+
         WebElement mini = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[2]/button[5]"));
         mini.click();
+        Thread.sleep(3500);
+
         /*Thread.sleep(2000);
         WebElement mini_select = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[22]/div"));
         Actions actions = new Actions(driver);
@@ -147,20 +168,11 @@ public class Video_Player {
         // and it isn't working
     }
 
-    @Test(description = "Test 11 Theatre mode")
-    public void Theatre() throws InterruptedException {
-        Thread.sleep(7000);
-        WebElement theatre = driver.findElement(By.xpath("//*[@id=\"movie_player\"]/div[38]/div[2]/div[2]/button[7]"));
-        theatre.click();
-        Thread.sleep(1000);
-        theatre.click();
-        //sometimes div[29] works, sometimes div[33] works idk why
-    }
     @AfterClass
     public void tearDown() throws InterruptedException {
         // Close the WebDriver
         if (driver != null) {
-            Thread.sleep(3000);
+            Thread.sleep(2000);
             driver.quit();
         }
     }
